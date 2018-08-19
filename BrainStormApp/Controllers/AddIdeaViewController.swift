c50e3fb3e5616d9ab5f8e2a52360b9a7883dbc7c
PKartavkin <pavel@macbook-pro-pavel.local>
2018-08-19 20:13:04 +0300

    added star rating feature

--- BrainStormApp/Controllers/AddIdeaViewController.swift
+++ BrainStormApp/Controllers/AddIdeaViewController.swift
@@ -12,10 +12,6 @@ import KMPlaceholderTextView
 
 class AddIdeaViewController: UIViewController {
     
-    private enum Constant {
-        static let ratings: [Int] = Array(1...10)
-    }
-    
     @IBOutlet weak var categoryButton: UIButton!
     
     private var selectedCategory: Category? {
@@ -26,6 +22,11 @@ class AddIdeaViewController: UIViewController {
             categoryButton.titleLabel?.text = selectedCategory.title
         }
     }
+    
+    @IBOutlet weak var timeToMarketLabel: UILabel!
+    @IBOutlet weak var requiredMoneyLabel: UILabel!
+    @IBOutlet weak var expectedProfitLabel: UILabel!
+    @IBOutlet weak var difficultyLabel: UILabel!
     
     @IBOutlet weak var timeToMarketCosmosView: CosmosView!
     @IBOutlet weak var requiredMoneyCosmosView: CosmosView!
@@ -40,17 +41,33 @@ class AddIdeaViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
     
-    // MARK: - Private
-    
-    private func showStandartPicker(fieldName: String, fieldButton: UIButton, completionHandler: ((Int) -> Void)? = nil) {
-        ActionSheetStringPicker.show(withTitle: fieldName, rows: Constant.ratings, initialSelection: 0, doneBlock: { picker, index, value in
-            var title = fieldName
-            title = "\(value ?? title)"
-            completionHandler?(Int(title)!)
-            
-            fieldButton.setTitle(title, for: .normal)
-            
-        }, cancel: {ActionStringCancelBlock in return}, origin: fieldButton)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timeToMarketCosmosView.rating = 5
+        requiredMoneyCosmosView.rating = 5
+        profitCosmosView.rating = 5
+        difficultyCosmosView.rating = 5
+        
+        timeToMarketCosmosView?.didTouchCosmos =
+            {(rating:Double)->() in
+                self.timeToMarketCosmosView.rating = rating.rounded(.up)
+                self.timeToMarketLabel.text = String(Int(rating.rounded(.up)))}
+        
+        requiredMoneyCosmosView?.didTouchCosmos =
+            {(rating:Double)->() in
+                self.requiredMoneyCosmosView.rating = rating.rounded(.up)
+                self.requiredMoneyLabel.text = String(Int(rating.rounded(.up)))}
+        
+        profitCosmosView?.didTouchCosmos =
+            {(rating:Double)->() in
+                self.profitCosmosView.rating = rating.rounded(.up)
+                self.expectedProfitLabel.text = String(Int(rating.rounded(.up)))}
+        
+        difficultyCosmosView?.didTouchCosmos =
+            {(rating:Double)->() in
+                self.difficultyCosmosView.rating = rating.rounded(.up)
+                self.difficultyLabel.text = String(Int(rating.rounded(.up)))}
     }
     
     private func calculateScore(timeToMarket: Int, requiredMoney: Int, expectableProfit: Int, difficulty: Int) -> Double {
@@ -63,8 +80,6 @@ class AddIdeaViewController: UIViewController {
         return scoreRounded
     }
     
-    // MARK: - IBAction
-    
     @IBAction func selectCategory(_ sender: UIButton) {
         UIHelper.showCategoryAction(from: self) { [weak self] category in
             guard let category = category else {
@@ -74,50 +89,14 @@ class AddIdeaViewController: UIViewController {
         }
     }
     
-    @IBAction func timeToMarket(_ sender: UIButton) {
-        showStandartPicker(fieldName: "Time To Market",
-                           fieldButton: sender) { [weak self] (count) in
-                            self?.timeToMarketCosmosView.rating = Double(count)
-        }
-    }
-    
-    @IBAction func requiredMoney(_ sender: UIButton) {
-        showStandartPicker(fieldName: "Required Money",
-                           fieldButton: sender) { [weak self] (count) in
-                            self?.requiredMoneyCosmosView.rating = Double(count)
-        }
-    }
-    
-    @IBAction func expectableProfit(_ sender: UIButton) {
-        showStandartPicker(fieldName: "Expectable Profit",
-                           fieldButton: sender) { [weak self] (count) in
-                            self?.profitCosmosView.rating = Double(count)
-        }
-    }
-    
-    @IBAction func difficulty(_ sender: UIButton) {
-        showStandartPicker(fieldName: "Difficulty",
-                           fieldButton: sender) { [weak self] (count) in
-                            self?.difficultyCosmosView.rating = Double(count)
-        }
-    }
-    
     @IBAction func done(_ sender: UIBarButtonItem) {
         guard let title = titleTextField.text, !title.isEmpty else {
             UIHelper.showErrorAlert(with: "Enter title please")
             return
         }
         
-        guard timeToMarketCosmosView.rating != 0
-            && requiredMoneyCosmosView.rating != 0
-            && profitCosmosView.rating != 0
-            && difficultyCosmosView.rating != 0 else {
-                UIHelper.showErrorAlert(with: "Empty ratings")
-                return
-        }
-        
         guard let description = descriptionTextView.text, !description.isEmpty else {
-            UIHelper.showErrorAlert(with: "Enter description pls")
+            UIHelper.showErrorAlert(with: "Enter description please")
             return
         }
         
