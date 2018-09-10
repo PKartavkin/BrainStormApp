446197d1e4c2388fe18bdbb7cb19ed4e6909b67c
PKartavkin <pavel@macbook-pro-pavel.local>
2018-09-10 10:19:13 +0300

    Fixed issue with initializing categories.

--- BrainStormApp/Extensions/AppInitialization.swift
+++ BrainStormApp/Extensions/AppInitialization.swift
@@ -22,13 +22,4 @@ extension AppDelegate {
             
         }
     }
-    
-    func presetupCategories() {
-        if DatabaseManager.categories().isEmpty {
-            DatabaseManager.addCategory("Media", color: .blue, colorName: "Blue", icon: #imageLiteral(resourceName: "blue_category_icon"))
-            DatabaseManager.addCategory("Dropshipping", color: .green, colorName: "Green", icon: #imageLiteral(resourceName: "green_category_icon"))
-            DatabaseManager.addCategory("Mobile Games", color: .orange, colorName: "Orange", icon: #imageLiteral(resourceName: "orange_category_icon"))
-            DatabaseManager.addCategory("Manufacturing", color: Color.yellow, colorName: "Yellow", icon: #imageLiteral(resourceName: "yellow_category_icon"))
-        }
-    }
 }
